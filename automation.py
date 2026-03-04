a3e1110d3f087096875ce9f5c4d633ec65800690
Ayush Mishra <officialayushmishra002@gmail.com>
2026-03-04 21:39:48 +0530

    little automation

---
+++ automation.py
@@ -0,0 +1,23 @@
+import pyautogui as s
+import time
+
+s.press("win")
+time.sleep(1)
+
+# s.write("notepad", interval = 0.2)
+# s.press("enter")
+
+# time.sleep(1)
+
+# s.write("Hello World", interval= 0.2)
+# s.press("enter")
+# s.write("You did it...", interval=0.1)
+# s.press("enter")
+
+s.write("comet", interval=0.1)
+s.press("enter")
+time.sleep(1)
+s.write("you tube", interval=0.3)
+s.press("enter",interval=5)
+s.write("open yt", interval=0.2)
+s.press("enter")
